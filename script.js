916ff61b1790352204a1fbd4abff342b29d7c285
addyufs <addyufs@gmail.com>
2022-06-10 10:52:55 +0300

    First CoMMit

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnOpenModal);
+
+for (let i =0; i < btnOpenModal.length; i++)
+btnOpenModal[i].addEventListener('click', function (){
+    // console.log('Clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+})
+
+btnCloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
+
+overlay.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
+
+document.addEventListener('keydown', function (e) {
+    // console.log(e.key);
+
+    if(e.key === 'Escape') {
+        if(!modal.classList.contains('hidden')) {
+            modal.classList.add('hidden');
+            overlay.classList.add('hidden');
+        }
+    }
+    
+})
